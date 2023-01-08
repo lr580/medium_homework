6fe542db8d01303fd671a8c28b7f1a15b28864b6
yjx <1797917338@qq.com>
2023-01-08 15:22:17 +0800

    新增公司的增删改查前端

--- browser/src/views/Company.vue
+++ browser/src/views/Company.vue
@@ -17,6 +17,16 @@
         <el-table-column prop="company" label="公司名"></el-table-column>
         <el-table-column prop="address" label="地址"></el-table-column>
         <el-table-column prop="companyInf" label="简介"></el-table-column>
+        <el-table-column label="操作" width="220" align="center" :hidden="false">
+          <template #default="scope">
+            <el-button text :icon="Edit" @click="handleEdit(scope.$index, scope.row)">
+              修改
+            </el-button>
+            <el-button text :icon="Delete" @click="handleDelete(scope.row.companyId)">
+              删除
+            </el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <div class="pagination">
         <el-pagination
@@ -27,18 +37,68 @@
           :total="pageTotal"
           @current-change="handlePageChange"
         ></el-pagination>
+        <el-button type="primary" icon="Plus" @click="handleAdd" :hidden="0">添加</el-button>
       </div>
     </div>
+
+    <!-- 编辑公司信息弹出框 -->
+    <el-dialog title="编辑公司" v-model="editVisible" width="30%">
+      <el-form label-width="70px">
+        <el-form-item label="公司名称">
+          <el-input v-model="form.company"></el-input>
+        </el-form-item>
+        <el-form-item label="公司地址">
+          <el-input v-model="form.address"></el-input>
+        </el-form-item>
+        <el-form-item label="简介">
+          <el-input v-model="form.companyInf"></el-input>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+				<span class="dialog-footer">
+					<el-button @click="editVisible = false">取 消</el-button>
+					<el-button type="primary" @click="saveEdit">确 定</el-button>
+				</span>
+      </template>
+    </el-dialog>
+
+
+    <!-- 添加公司信息弹出框 -->
+    <el-dialog title="添加公司" v-model="addVisible" width="30%">
+      <el-form label-width="100px">
+        <el-form-item label="公司ID">
+          <el-input v-model="form.companyId"></el-input>
+        </el-form-item>
+        <el-form-item label="公司名称">
+          <el-input v-model="form.company"></el-input>
+        </el-form-item>
+        <el-form-item label="公司地址">
+          <el-input v-model="form.address"></el-input>
+        </el-form-item>
+        <el-form-item label="公司简介">
+          <el-input v-model="form.companyInf"></el-input>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+				<span class="dialog-footer">
+					<el-button @click="addVisible = false">取 消</el-button>
+					<el-button type="primary" @click="saveAdd">确 定</el-button>
+				</span>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
 <script setup>
 import {reactive, ref} from "vue";
-import {findCompany, getAllCompany} from "@/api";
+import {addCompany, addExpress, deleteCompany, findCompany, getAllCompany, updateCompany} from "@/api";
 import {ElMessage} from "element-plus";
-
+import {Delete, Edit} from "@element-plus/icons-vue";
 const searchId = ref()
-
+// 表格编辑时弹窗和保存
+const editVisible = ref(false)
+// 表格添加时弹窗和保存
+const addVisible = ref(false)
 const tableData = ref([]);
 const query = reactive({
   current: 1,
@@ -50,6 +110,86 @@ const handlePageChange = (val) => {
   query.current = val;
   getData();
 };
+
+//删除公司信息
+const handleDelete = (companyId) =>{
+  deleteCompany(companyId).then(res => {
+    if (res.data.code === 10000) {
+      ElMessage.success("删除成功")
+      getData()
+    } else {
+      ElMessage.error("删除失败：" + res.data.info)
+    }
+  }).catch(err => {
+    ElMessage.error("删除失败：" + err.message)
+  })
+
+}
+
+
+let form = reactive({
+  companyId: -1,
+  company: '',
+  address: '',
+  companyInf: ''
+})
+let idx = -1;
+//处理更新事件
+const handleEdit = (index, row) => {
+  idx = index;
+  form.companyId = row.companyId,
+  form.company = row.company
+  form.address = row.address
+  form.companyInf = row.companyInf
+  editVisible.value = true
+}
+//更新编辑后的信息
+const saveEdit = () =>{
+  updateCompany(form).then(res => {
+    if (res.data.code === 10000) {
+      ElMessage.success(`修改第 ${idx + 1} 行成功`);
+      if (form.company !== '') {
+        tableData.value[idx].company = form.company
+      }
+      if (form.address !== '') {
+        tableData.value[idx].address = form.address
+      }
+      if (form.companyInf !== '') {
+        tableData.value[idx].companyInf = form.companyInf
+      }
+      editVisible.value = false
+    } else {
+      ElMessage.success(`修改失败` + res.data.info)
+    }
+  }).catch(err => {
+    ElMessage.success(`修改失败` + err.message)
+  })
+}
+
+//处理添加事件
+const handleAdd = () => {
+  form.companyId = null
+  form.company = ''
+  form.address = ''
+  form.companyInf = ''
+  addVisible.value = true
+}
+
+const saveAdd = () => {
+  addCompany(form).then(res => {
+    if (res.data.code === 10000) {
+      ElMessage.success("添加成功")
+      query.current = 1
+      getData()
+      addVisible.value = false
+    } else {
+      ElMessage.error("添加失败：" + res.data.info)
+    }
+  }).catch(err => {
+    ElMessage.error("添加失败：" + err.message)
+  })
+}
+
 
 // 获取表格数据
 const getData = () => {
--- server/src/main/java/com/example/express/mapper/express/ExpressMapper.java
+++ server/src/main/java/com/example/express/mapper/express/ExpressMapper.java
@@ -3,7 +3,6 @@ package com.example.express.mapper.express;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.express.entity.express.ExpressEntity;
 import org.apache.ibatis.annotations.Mapper;
-
 /**
  * .
  *
